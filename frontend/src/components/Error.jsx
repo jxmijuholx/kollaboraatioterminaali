36178c9a028e5e712b44303b08c1ef0c29c2337f
Tuutej <90647045+Tuutej@users.noreply.github.com>
2024-11-18 14:13:32 +0200

    upgraded error message

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -6,15 +6,22 @@ function Error() {
 
     return (
         <>
-            <p>Something went wrong sadge</p>
-            <Link to={"/"}>
-                <Button
-                    variant='contained'
-                    color='success'
-                >
-                    Back to home page
-                </Button>
-            </Link>
+            <p>Something went wrong</p>
+            <img 
+                src="./src/assets/Sadge.png" 
+                alt="sadge" 
+                style={{ width: '75px', height: 'auto', margin: '20px 0' }} 
+            />
+            <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                <Link to={"/"}>
+                    <Button
+                        variant='contained'
+                        color='success'
+                    >
+                        Back to home page
+                    </Button>
+                </Link>
+            </div>
         </>
     );
 }
